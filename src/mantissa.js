b7cf2ccfa261c3feaf4496bcd72e0674317ead75
Karl Koscher <supersat@cs.washington.edu>
2018-09-16 03:46:42 -0700

    Fix mantissa decoding

--- src/mantissa.js
+++ src/mantissa.js
@@ -1,31 +1,34 @@
 import { BAP_1, BAP_2, BAP_4, BAP_5 } from "./tables";
 
 export const MantissaReader = function(frame) {
-    this.bap_1 = [];
-    this.bap_2 = [];
-    this.bap_4 = [];
+    this.bap_1_ptr = 3;
+    this.bap_2_ptr = 3;
+    this.bap_4_ptr = 2;
 
     this.get = function(bap) {
         switch (bap) {
             case 0:
                 return 0;
             case 1:
-                if (this.bap_1 === []) {
+                if (this.bap_1_ptr > 2) {
                     this.bap_1 = BAP_1[frame.getUnsigned(5)];
+                    this.bap_1_ptr = 0;
                 }
-                return this.bap_1.shift();
+                return this.bap_1[this.bap_1_ptr++];
             case 2:
-                if (this.bap_2 === []) {
+                if (this.bap_2_ptr > 2) {
                     this.bap_2 = BAP_2[frame.getUnsigned(7)];
+                    this.bap_2_ptr = 0;
                 }
-                return this.bap_2.shift();
+                return this.bap_2[this.bap_2_ptr++];
             case 3:
-                return frame.getUnsigned(3);
+                return frame.getSigned(3) / 7;
             case 4:
-                if (this.bap_4 === []) {
+                if (this.bap_4_ptr > 1) {
                     this.bap_4 = BAP_4[frame.getUnsigned(7)];
+                    this.bap_4_ptr = 0;
                 }
-                return this.bap_4.shift();
+                return this.bap_4[this.bap_4_ptr++];
             case 5:
                 return BAP_5[frame.getUnsigned(4)];
             case 6:
@@ -36,11 +39,13 @@ export const MantissaReader = function(frame) {
             case 11:
             case 12:
             case 13:
-                return frame.getUnsigned(bap - 1) / Math.pow(2, bap - 1);
+                return frame.getSigned(bap - 1) / Math.pow(2, bap - 2);
             case 14:
-                return frame.getUnsigned(14) / Math.pow(2, 14);
+                return frame.getSigned(14) / Math.pow(2, 13);
             case 15:
-                return frame.getUnsigned(16) / Math.pow(2, 16);
+                return frame.getSigned(16) / Math.pow(2, 15);
+            default:
+                debugger;
         }
     }
 }
