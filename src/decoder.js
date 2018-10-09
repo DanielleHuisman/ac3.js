f1aed2e04501ac87987ab2c19814a3db13c09287
Daniel Huisman <daniel@huisman.me>
2018-10-09 03:15:21 +0200

    Add Heavy Compression and fix Dynamic Range Control

--- src/decoder.js
+++ src/decoder.js
@@ -59,27 +59,53 @@ export class AC3Decoder extends Decoder {
 
         // Store the new Dynamic Range Control levels if present
         if (bsi.dynrng !== undefined) {
-            this.dynrng = Math.pow(2, ((bsi.dynrng & 0x80) !== 0 ? -4 + (bsi.dynrng & 0x60) : bsi.dynrng & 0x60) + 1);
+            this.dynrng = (bsi.dynrng & 0x60) >> 5;
+            this.dynrng = Math.pow(2, ((bsi.dynrng & 0x80) !== 0 ? -4 + this.dynrng : this.dynrng & 0x60) + 1);
             this.dynrng += ((bsi.dynrng & 0x1f) + 32) / 64;
         }
         if (bsi.dynrng2 !== undefined) {
-            this.dynrng2 = Math.pow(2, ((bsi.dynrng2 & 0x80) !== 0 ? -4 + (bsi.dynrng2 & 0x60) : bsi.dynrng2 & 0x60) + 1);
+            this.dynrng2 = (bsi.dynrng2 & 0x60) >> 5;
+            this.dynrng2 = Math.pow(2, ((bsi.dynrng2 & 0x80) !== 0 ? -4 + (this.dynrng2 & 0x60) : this.dynrng2 & 0x60) + 1);
             this.dynrng2 += ((bsi.dynrng2 & 0x1f) + 32) / 64;
         }
 
         // Dynamic Range Control
         for (let i = 0; i < 1536; i++) {
             if (bsi.acmod === 0 || bsi.acmod === 2) {
-                this.samples[0][i] *= this.dynrng;
-                this.samples[1][i] *= this.dynrng2;
+                this.samples[0][i] *= this.dynrng === 0 ? 1 : this.dynrng;
+                this.samples[1][i] *= this.dynrng2 === 0 ? 1 : this.dynrng2;
             } else {
                 for (let ch = 0; ch < bsi.nfchans; ch++) {
-                    this.samples[ch][i] *= this.dynrng;
+                    this.samples[ch][i] *= this.dynrng === 0 ? 1 : this.dynrng;
                 }
             }
         }
 
-        // TODO: Heavy Compression
+
+        // Heavy Compression
+        if (bsi.compr !== undefined) {
+            let compr = (bsi.compr & 0x70) >> 4;
+            compr = Math.pow(2, ((bsi.compr & 0x80) !== 0 ? -8 + compr : compr) + 1);
+            compr += ((bsi.compr & 0x0f) + 16) / 32;
+
+            let compr2 = undefined;
+            if (bsi.compr2 !== undefined) {
+                compr2 = (bsi.compr2 & 0x70) >> 4;
+                compr2 = Math.pow(2, ((bsi.compr2 & 0x80) !== 0 ? -8 + compr : compr2) + 1);
+                compr2 += ((bsi.compr2 & 0x0f) + 16) / 32;
+            }
+
+            for (let i = 0; i < 1536; i++) {
+                if (bsi.acmod === 0 || bsi.acmod === 2) {
+                    this.samples[0][i] *= compr ? compr : 1;
+                    this.samples[1][i] *= compr2 ? compr2 : 1;
+                } else {
+                    for (let ch = 0; ch < bsi.nfchans; ch++) {
+                        this.samples[ch][i] *= compr ? compr : 1;
+                    }
+                }
+            }
+        }
 
         // Downmixing
         downmix(bsi, this.samples);
